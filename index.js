951bb88e8f563338e82f7d64eafcb31be9e7570f
Shaxriyor <shaxriyorturgunboyovshaxriyor@gmail.com>
2025-02-11 20:04:50 +0500

    first commit

--- index.js
+++ index.js
@@ -27,6 +27,45 @@ const foot = document.querySelector('.foot');
 const btn1 = document.querySelector('.btn1');
 let lorem = 'Shaxriyor turgunboyov oziga hos bolgan narsalarni ajratib berilag bolasa';
 btn1 === null || btn1 === void 0 ? void 0 : btn1.addEventListener('click', () => {
-    (foot === null || foot === void 0 ? void 0 : foot.innerHTML) === lorem;
+    (foot === null || foot === void 0 ? void 0 : foot.textContent) === lorem;
+});
+// let a = 'Shaxriyor';
+// a = 10;
+let f = 'Shaxriyor';
+f = 'Abdulaziz';
+console.log(f);
+let g = 30;
+g = 16;
+console.log(g);
+// let s = false
+// s = true
+let k = false;
+console.log(k);
+// let o = {}
+// o.name
+// o = {'Shaxriyor'}
+let q = { name: 'Shaxriyor' };
+console.log(q);
+let p;
+p = 12;
+p = 'Shaxriyor';
+p = false;
+p = { name: 'Shaxriyor' };
+p = [];
+p = null;
+console.log(p);
+let z = { name: 'Shaxriyor', age: 30 };
+console.log(z);
+let son = { Familya: 'Turg\'unboyov', name: 'Shaxriyor', age: 17, qiymati: true };
+console.log(son);
+let familya = 'Turgunboyov';
+let ismim = 'Shaxriyor';
+let age = 17;
+console.log(familya + ' ' + ' ' + ismim + ' ' + age);
+const button = document.querySelector(".btn-2");
+const output = document.querySelector(".liko");
+let lorems = "Salom mening ismmim shaxriyor men hozirda 17 yoshdan men hozir 5-maktabda oqiyman va o'zimni dasturlash soham boyicha yaxshi tayorlanaman va kelajakda kattta kampanya ochaman google , nasa , facebookga oxshagan";
+button.addEventListener('click', () => {
+    output.textContent = lorems;
 });
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -31,9 +31,72 @@ btn?.addEventListener('click' , ()=>{
   window.location.href = '/pages/Home.html'
 })
 
-const foot = document.querySelector('.foot')
-const btn1 = document.querySelector('.btn1');
-let lorem = 'Shaxriyor turgunboyov oziga hos bolgan narsalarni ajratib berilag bolasa'
+const foot = document.querySelector('.foot') as HTMLParagraphElement;
+const btn1 = document.querySelector('.btn1') as HTMLButtonElement;
+let lorem: string = 'Shaxriyor turgunboyov oziga hos bolgan narsalarni ajratib berilag bolasa';
 btn1?.addEventListener('click' , ()=>{
-     foot?.innerHTML === lorem
-})
+     foot?.textContent === lorem
+});
+
+
+
+// let a = 'Shaxriyor';
+// a = 10;
+
+let f: string = 'Shaxriyor';
+f = 'Abdulaziz';
+console.log(f);
+
+
+let g: Number = 30;
+g = 16;
+console.log(g);
+
+// let s = false
+// s = true
+
+let k: boolean = false;
+console.log(k);
+
+
+// let o = {}
+// o.name
+// o = {'Shaxriyor'}
+
+
+let q: {name : string} = {name : 'Shaxriyor'};
+console.log(q);
+
+
+let p: any;
+p = 12;
+p = 'Shaxriyor';
+p = false;
+p = { name : 'Shaxriyor'};
+p = [];
+p = null;
+console.log(p);
+
+
+let z: {name : string , age : number} = {name : 'Shaxriyor' , age : 30}
+console.log(z);
+
+
+let son: {Familya : string , name : string , age : number , qiymati : boolean} = {Familya : 'Turg\'unboyov' , name : 'Shaxriyor' , age : 17 , qiymati : true}
+console.log(son);
+
+let familya: string = 'Turgunboyov';
+let ismim : string = 'Shaxriyor';
+let age : number = 17;
+console.log(familya + ' ' + ' ' + ismim  + ' ' +  age);
+
+
+
+const button = document.querySelector(".btn-2") as HTMLButtonElement;
+const output = document.querySelector(".liko") as HTMLParagraphElement;
+
+let lorems: string = "Salom mening ismmim shaxriyor men hozirda 17 yoshdan men hozir 5-maktabda oqiyman va o'zimni dasturlash soham boyicha yaxshi tayorlanaman va kelajakda kattta kampanya ochaman google , nasa , facebookga oxshagan"
+
+button.addEventListener('click' , ()=>{
+     output.textContent = lorems
+});
